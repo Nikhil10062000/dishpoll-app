d635dec757742385142c578a25a75fb05b7fb906
unik-10 <94104092+Nikhil10062000@users.noreply.github.com>
2022-08-27 11:58:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+
+
+
 import React,{useState,useEffect} from 'react';
 import './vote.js';
-
-const App=()=>{
+import {Form , Button} from 'react-bootstrap'
+ const App=()=>{
   const [data,setData] =useState([])
- 
+  const [login , setLogin] = useState(true)
       const getdata=async()=>{
         const response=await fetch('https://raw.githubusercontent.com/syook/react-dishpoll/main/db.json');
         setData(await response.json());
@@ -16,9 +19,26 @@ const App=()=>{
     
   
   return (
-  
+     <>
+
+      {
+
+        login ? (<Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="user" placeholder="Enter user" />
+            
+          </Form.Group>
     
-    <div className="container-fluid mt-5">
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          
+          <Button variant="primary" type="submit" onClick={()=> setLogin(false)}>
+            Submit
+          </Button>
+        </Form>) : (  <div className="container-fluid mt-5">
     <div className="row "><h2>DISH POLL</h2>
   
     {
@@ -29,7 +49,7 @@ const App=()=>{
     <div className="col mt-5">
       <div className="card p-1">
         <div className="d-flex align-items-center">
-          <div className="image"><img src={curElem.image} class="rounded" width="160"/></div>
+          <div className="image"><img src={curElem.image} alt="images" class="rounded" width="160"/></div>
           <div className="ml-3 w-100">
             <h4 className="mb-0 mt-0 textLeft">{curElem.dishName}</h4><span className="textLeft"key={curElem.id}></span>
             <div className="p-2 mt-2 bg-secondary d-flex justify-content-between rounded text-black status">
@@ -54,6 +74,18 @@ const App=()=>{
     }
   </div>
 </div>
+
+        )
+      }
+
+     </>
+
+
+
+
+   
+     
+    
 
    
   );
